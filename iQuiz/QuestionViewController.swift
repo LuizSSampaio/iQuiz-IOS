71273eeb28b714789783bc68d3550c9812805321
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2023-07-28 18:49:58 -0300

    🎨 Style: Added Question Interface

--- iQuiz/QuestionViewController.swift
+++ iQuiz/QuestionViewController.swift
@@ -9,21 +9,15 @@ import UIKit
 
 class QuestionViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var questionTitleLabel: UILabel!
+    
+    @IBAction func responseButtonPressed(_ sender: UIButton) {
+        print(sender.tag)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.hidesBackButton = true
     }
-    */
 
 }
--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -8,19 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var startButton: UIButton!
-    
-    @IBAction func startButtonPressed(_ sender: Any) {
-        print("Button pressed")
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureLayout()
-    }
-    
-    func configureLayout() {
-        startButton.layer.cornerRadius = 32.0
     }
 }
 
